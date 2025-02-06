b154c40cd6ad1ca820cdff7563d76f6bb50637f0
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-06 20:54:58 +0000

    add search and error handling func

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import './style.css';
 
 const img = document.querySelector('img');
-fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=lzAul6xT8c6v3LiMP3nznRA1fHNrFHRe&s=cats',
-  {
-    mode: 'cors',
-  },
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+const refreshBtn = document.getElementById('refreshBtn');
+const searchBtn = document.getElementById('seachBtn');
+const searchContent = document.getElementById('gifSearch');
+const errorMessage = document.querySelector('.errorMsg');
+
+function newGif(input) {
+  let searchTerm = 'cats';
+  if (input !== undefined) {
+    searchTerm = input;
+  }
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=lzAul6xT8c6v3LiMP3nznRA1fHNrFHRe&s=${searchTerm}`,
+    {
+      mode: 'cors',
+    },
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      if (response.meta.status === 200 && response.data.length === 0) {
+        throw new Error(`couldn't find search term`);
+      }
+      if (!response.ok) {
+        throw new Error(response.meta.msg);
+      }
+      console.log(response);
+      img.src = response.data.images.original.url;
+    })
+    .catch(function (e) {
+      console.error(e);
+      errorMessage.textContent = e;
+    });
+}
+newGif();
+refreshBtn.addEventListener('click', () => newGif());
+searchBtn.addEventListener('click', () => newGif(searchContent.value));
